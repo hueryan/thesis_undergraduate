bf5b13583b07bdbc57679f13f79a4d29f8ae24d9
hujing <1527262047@qq.com>
2025-05-10 20:37:03 +0800

    update_config

--- pdf_knowledge_chat/config.py
+++ pdf_knowledge_chat/config.py
@@ -1,6 +1,6 @@
 from dotenv import dotenv_values
 
-configs = dotenv_values(".env")
+configs = dotenv_values("milvus_config.env")
 DEEPSEEK_API_KEY = configs['DEEPSEEK_API_KEY']
 MODEL_NAME = configs['MODEL_NAME']
 DB_NAME = configs['DB_NAME']
