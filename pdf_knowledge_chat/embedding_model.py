7669e96a56e93891eea053a91b9525f6331a3b78
hujing <1527262047@qq.com>
2025-05-12 17:23:38 +0800

    指定本地模型

--- pdf_knowledge_chat/embedding_model.py
+++ pdf_knowledge_chat/embedding_model.py
@@ -1,5 +1,10 @@
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from config_milvus import MODEL_NAME
+import os
+# 获取当前脚本所在目录（即 configs 目录）
+current_dir = os.path.dirname(os.path.abspath(__file__))
+LOCAL_MODEL_PATH = f"{current_dir}/{MODEL_NAME}"
+
 
 MODEL_DIM_MAPPING = {
     "BAAI/bge-small-zh-v1.5": 512,
@@ -11,7 +16,7 @@ model_kwargs = {'device': 'cuda'}
 encode_kwargs = {'normalize_embeddings': True}
 
 embedding_model = HuggingFaceBgeEmbeddings(
-    model_name=MODEL_NAME,
+    model_name=LOCAL_MODEL_PATH,
     model_kwargs=model_kwargs,
     encode_kwargs=encode_kwargs,
     query_instruction="为以下问题生成表示以检索相关数据结构学习资料：",
